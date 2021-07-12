10edfffe32e0e6f0e8d7acafe483eb7f33b25f4b
Joey Gold <joey.gold@softwire.com>
2021-07-12 13:52:50 +0100

    king moves one square in any direction, Red-7 passed

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +7,26 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0)
+        const availableMoves = [];
+        const position = board.findPiece(this);
+        const modifiers = King.getKingModifers();
+            for (var modifier of modifiers){
+                var possibleSquare = Square.at(position.row+modifier.row, position.col+modifier.col)
+                if (possibleSquare.checkOnBoard()) {
+                    availableMoves.push(possibleSquare);
+                }
+            }
+        return availableMoves;
+    }
+
+    static getKingModifers(){
+        var modifiers = []
+        for (var i of [-1,0,1]){
+            for (var j of [-1,0,1]){
+                if(i !== 0 || j !==0){
+                modifiers.push(Square.at(i,j));
+            }}
+        }
+        return modifiers;
     }
 }

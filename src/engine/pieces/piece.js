4f1b8b35459963606845c877ee787bca0c6729b6
Joey Gold <joey.gold@softwire.com>
2021-07-12 17:33:35 +0100

    function that checks whether a piece checks a king works

--- src/engine/pieces/piece.js
+++ src/engine/pieces/piece.js
@@ -1,3 +1,5 @@
+import Square from "../square";
+
 export default class Piece {
     constructor(player) {
         this.player = player;
@@ -11,6 +13,25 @@ export default class Piece {
     moveTo(board, newSquare) {
         const currentSquare = board.findPiece(this);
         board.movePiece(currentSquare, newSquare);
+    }
+
+    checks(board) {
+        var kingPosition;
+    for(var i=0; i<8; i++) {
+        for(var j=0; j<8; j++){
+            if(!!board.getPiece(Square.at(i,j)) && board.getPiece(Square.at(i,j)).name == 'king' && board.getPiece(Square.at(i,j)).player != this.player) {
+                kingPosition = Square.at(i,j);
+            }
+        }
+        
+    }
+    const possibleMoves = this.getAvailableMoves(board);
+    for(var move in possibleMoves) {
+        if (move.equals(kingPosition)) {
+            return true;
+        }
+    }
+    return false;
     }
 
 }

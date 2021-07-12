a460d8bb38e52a56a095bb7f7d150b58906facec
Joey Gold <joey.gold@softwire.com>
2021-07-12 11:34:42 +0100

    Pawns can move 2 spaces on first move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,11 +11,12 @@ export default class Pawn extends Piece {
         console.log(this.player);
         const availableMoves = [];
         const position = board.findPiece(this);
-        if (this.player === Player.WHITE) {
-            availableMoves.push(Square.at(position.row +1 , position.col));
-        };
-        if (this.player === Player.BLACK) {
-            availableMoves.push(Array(Square.at(position.row -1 , position.col)));
+        const moveDir = (this.player === Player.WHITE ? 1 : -1);
+        availableMoves.push(Square.at(position.row +1*moveDir , position.col));
+        const linearA = 3.5;
+        const linearB = -2.5;
+        if (position.row==Math.round(linearA+moveDir*linearB)){
+            availableMoves.push(Square.at(position.row +2 * moveDir, position.col));
         }
         return availableMoves;
     }
